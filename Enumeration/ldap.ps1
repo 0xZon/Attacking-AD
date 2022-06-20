736879bffe60bd68ae8f9801e5681915eb868e32
Aaron Haymore <52760493+0xZon@users.noreply.github.com>
2022-06-20 16:45:02 -0600

    Create ldap.ps1

---
+++ Enumeration/ldap.ps1
@@ -0,0 +1,38 @@
+#Get LDAP Path
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() #This will get the domain name and domain controller 
+
+$PDC = ($domainObj.PdcRoleOwner).Name #Save the name of the domain controller into PDC i.e. DC01.corp.com
+
+#The next few lines will build a LDAP query -- LDAP://HostName[:PortNumber][/DistinguishedName]
+$SearchString = "LDAP://" 
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))" # This is chaning corp.com to look like DC=corp,DC=com
+
+$SearchString += $DistinguishedName
+
+$SearchString #This will give us the full LDAP provider path LDAP://DC01.corp.com/DC=corp,DC=com
+
+
+#Use LDAP path with DirectorySearcher to start doing queries 
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry($SearchString, "corp.com\offsec", "lab") #Authentication in order to query LDAP
+
+$Searcher.SearchRoot = $objDomain
+
+#Filters so we dont get spammed with data from LDAP
+$Searcher.filter="samAccountType=805306368" # 0x30000000 (decimal 805306368) used to enumerate all users
+
+$Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+    Foreach($prop in $obj.Properties)
+    {
+        $prop
+    }
+    
+    Write-Host "------------------------"
+}
